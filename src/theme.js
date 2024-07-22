624c5693888101064f6368302c6ba9544467f732
Nguyen Dinh Hung <matrix16hung@gmail.com>
2024-07-22 23:56:55 +0300

    version22 override(custom) theme mui app_bar

--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,6 @@
 import { experimental_extendTheme as extendTheme } from "@mui/material/styles";
 import { teal, deepOrange, cyan, orange } from "@mui/material/colors";
+import { BorderColor } from "@mui/icons-material";
 
 // Create a theme instance.
 const theme = extendTheme({
@@ -18,6 +19,45 @@ const theme = extendTheme({
       palette: {
         primary: cyan,
         secondary: orange,
+      },
+    },
+  },
+  components: {
+    // Name of the component
+    MuiButton: {
+      styleOverrides: {
+        // Name of the slot
+        root: {
+          // Some CSS
+          textTransform: "none",
+        },
+      },
+    },
+    MuiInputLabel: {
+      styleOverrides: {
+        root: ({ theme }) => ({
+          color: theme.palette.primary.main,
+          fontSize: "0.875rem",
+        }),
+      },
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: ({ theme }) => ({
+          color: theme.palette.primary.main,
+          fontSize: "0.875rem",
+          ".MuiOutlinedInput-notchedOutline": {
+            borderColor: theme.palette.primary.light,
+          },
+          "&:hover": {
+            ".MuiOutlinedInput-notchedOutline": {
+              borderColor: theme.palette.primary.main,
+            },
+          },
+          "& fieldset": {
+            borderWidth: "1px !important",
+          },
+        }),
       },
     },
   },
