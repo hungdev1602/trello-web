faf918e8de9dbd884037eea2dbad7e6408685a9d
Nguyen Dinh Hung <matrix16hung@gmail.com>
2024-07-24 00:41:26 +0300

    version27 refactor code structure

---
+++ src/pages/Boards/BoardContent/ListColumns/Column/ListCards/Card/Card.jsx
@@ -0,0 +1,62 @@
+import { Card as MuiCard } from "@mui/material";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import GroupIcon from "@mui/icons-material/Group";
+import CommentIcon from "@mui/icons-material/Comment";
+import AttachmentIcon from "@mui/icons-material/Attachment";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+function Card({ temporaryHideMedia }) {
+  if (temporaryHideMedia) {
+    return (
+      <>
+        <MuiCard
+          sx={{
+            cursor: "pointer",
+            boxShadow: "0 1px 1px rgba(0, 0, 0, 0.2)",
+            overflow: "unset",
+          }}
+        >
+          <CardContent sx={{ p: 1.5, "&:last-child": { p: 1.5 } }}>
+            <Typography>Card test 01</Typography>
+          </CardContent>
+        </MuiCard>
+      </>
+    );
+  }
+  return (
+    <>
+      <MuiCard
+        sx={{
+          cursor: "pointer",
+          boxShadow: "0 1px 1px rgba(0, 0, 0, 0.2)",
+          overflow: "unset",
+        }}
+      >
+        <CardMedia
+          sx={{ height: 140 }}
+          image="https://t4.ftcdn.net/jpg/03/86/82/73/360_F_386827376_uWOOhKGk6A4UVL5imUBt20Bh8cmODqzx.jpg"
+          title="green iguana"
+        />
+        <CardContent sx={{ p: 1.5, "&:last-child": { p: 1.5 } }}>
+          <Typography>HungDev MERN Stack</Typography>
+        </CardContent>
+        <CardActions sx={{ p: "0 4px 8px 4px" }}>
+          <Button size="small" startIcon={<GroupIcon />}>
+            20
+          </Button>
+          <Button size="small" startIcon={<CommentIcon />}>
+            15
+          </Button>
+          <Button size="small" startIcon={<AttachmentIcon />}>
+            10
+          </Button>
+        </CardActions>
+      </MuiCard>
+    </>
+  );
+}
+
+export default Card;
