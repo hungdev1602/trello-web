02480d9d38a6d2e765195baf5110fbc0a4ff9ff2
Nguyen Dinh Hung <matrix16hung@gmail.com>
2024-07-24 16:23:03 +0300

    version30 dnd-kit kéo thả

--- src/pages/Boards/BoardContent/ListColumns/Column/Column.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Column/Column.jsx
@@ -19,7 +19,21 @@ import DragHandleIcon from "@mui/icons-material/DragHandle";
 import ListCards from "./ListCards/ListCards";
 import { mapOrder } from "~/utils/sort";
 
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+
 function Column({ column }) {
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({ id: column._id, data: { ...column } });
+
+  const dndKitColumnStyles = {
+    // touchAction: "none", //dành cho sensor default dạng PointerSensor
+    // touchAction: 'none', // Dành cho sensor default dạng PointerSenso
+    // Nếu sử dụng CSS.Transform như docs sẽ lỗi nếu stretch => đổi lại Translate
+    transform: CSS.Translate.toString(transform),
+    transition,
+  };
+
   const [anchorEl, setAnchorEl] = useState(null);
   const open = Boolean(anchorEl);
   const handleClick = (event) => {
@@ -30,10 +44,15 @@ function Column({ column }) {
   };
 
   const orderedCards = mapOrder(column?.cards, column?.cardOrderIds, "_id");
+
   return (
     <>
       {/* Box Column test 01 */}
       <Box
+        ref={setNodeRef}
+        style={dndKitColumnStyles}
+        {...attributes}
+        {...listeners}
         sx={{
           minWidth: "300px",
           maxWidth: "300px",
--- src/pages/Boards/BoardContent/ListColumns/ListColumns.jsx
+++ src/pages/Boards/BoardContent/ListColumns/ListColumns.jsx
@@ -2,50 +2,63 @@ import Box from "@mui/material/Box";
 import Column from "./Column/Column";
 import Button from "@mui/material/Button";
 import NoteAddIcon from "@mui/icons-material/NoteAdd";
+import {
+  SortableContext,
+  horizontalListSortingStrategy,
+} from "@dnd-kit/sortable";
 
 function ListColumns({ columns }) {
+  /*
+   * SortableContext yêu cầu items là 1 mảng dạng ["id-1", "id-2"] chứ ko phải [{id: "id-1"}]
+   * nếu ko đúng thì vẫn có thể kéo thả nhưng ko có animation
+   */
   return (
     <>
-      <Box
-        sx={{
-          bgcolor: "inherit",
-          width: "100%",
-          height: "100%",
-          display: "flex",
-          overflowX: "auto",
-          overflowY: "hidden",
-          "&::-webkit-scrollbar-track": { m: 2 },
-        }}
+      <SortableContext
+        items={columns?.map((c) => c._id)} //xử lý SortableContext lấy dạng nguyên thuỷ
+        strategy={horizontalListSortingStrategy}
       >
-        {columns?.map((column) => (
-          <Column key={column._id} column={column} />
-        ))}
-
-        {/* Box add new column CTA */}
         <Box
           sx={{
-            minWidth: "200px",
-            maxWidth: "200px",
-            mx: 2,
-            borderRadius: "6px",
-            height: "fit-content",
-            bgcolor: "#ffffff3d",
+            bgcolor: "inherit",
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            overflowX: "auto",
+            overflowY: "hidden",
+            "&::-webkit-scrollbar-track": { m: 2 },
           }}
         >
-          <Button
-            startIcon={<NoteAddIcon />}
+          {columns?.map((column) => (
+            <Column key={column._id} column={column} />
+          ))}
+
+          {/* Box add new column CTA */}
+          <Box
             sx={{
-              color: "white",
-              width: "100%",
-              justifyContent: "flex-start",
-              pl: 2.5,
-              py: 1,
+              minWidth: "200px",
+              maxWidth: "200px",
+              mx: 2,
+              borderRadius: "6px",
+              height: "fit-content",
+              bgcolor: "#ffffff3d",
             }}
           >
-            Add new column
-          </Button>
+            <Button
+              startIcon={<NoteAddIcon />}
+              sx={{
+                color: "white",
+                width: "100%",
+                justifyContent: "flex-start",
+                pl: 2.5,
+                py: 1,
+              }}
+            >
+              Add new column
+            </Button>
+          </Box>
         </Box>
-      </Box>
+      </SortableContext>
     </>
   );
 }
