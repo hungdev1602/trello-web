84f634676e1815e93fb8dcf9e734ebf69bd53cc6
Nguyen Dinh Hung <matrix16hung@gmail.com>
2024-07-29 11:44:57 +0300

    fix bug mouseSensor & touchSensor khi boi den

--- src/pages/Boards/BoardContent/ListColumns/Column/Column.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Column/Column.jsx
@@ -220,6 +220,7 @@ function Column({ column }) {
                   size="small"
                   variant="outlined"
                   autoFocus
+                  data-no-dnd="true"
                   value={newCardTitle}
                   onChange={(e) => setNewCardTitle(e.target.value)}
                   sx={{
@@ -250,6 +251,7 @@ function Column({ column }) {
                 />
                 <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
                   <Button
+                    data-no-dnd="true"
                     onClick={addNewCard}
                     variant="contained"
                     color="success"
